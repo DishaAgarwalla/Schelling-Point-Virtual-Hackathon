1ed01e83625e46ebce5e15fdb693348b1e32e2ad
Disha Agarwalla <dishaagarwalla2001@gmail.com>
2022-01-16 00:11:40 +0530

    video js added

--- src/pages/Holders.js
+++ src/pages/Holders.js
@@ -1,22 +1,52 @@
-import React from 'react';
-// import videojs from "video.js";
-// import "videojs-contrib-hls";
-// import "videojs-contrib-quality-levels";
-// import "videojs-hls-quality-selector";
-// import "video.js/dist/video-js.min.css";
+import React from "react";
+import VideoJS from "../components/VideoJS"; // point to where the functional component is stored
 
 const Holders = () => {
+  const playerRef = React.useRef(null);
 
-    
-    return (
-        <div>
-          
+  const videoJsOptions = {
+    // lookup the options in the docs for more options
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: "",
+      },
+    ],
+  };
 
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
 
-            
-        </div>
-    )
-}
+    // you can handle player events here
+    player.on("waiting", () => {
+      console.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      console.log("player will dispose");
+    });
+  };
+
+  // const changePlayerOptions = () => {
+  //   // you can update the player through the Video.js player instance
+  //   if (!playerRef.current) {
+  //     return;
+  //   }
+  //   // [update player through instance's api]
+  //   playerRef.current.src([{src: 'http://ex.com/video.mp4', type: 'video/mp4'}]);
+  //   playerRef.current.autoplay(false);
+  // };
+
+  return (
+    <>
+      <div>AMA Session</div>
+
+      <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+    </>
+  );
+};
 
 export default Holders;
-
